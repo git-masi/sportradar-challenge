82d842bd92c6d4b53b13a93b0325d066d0e63ddd
git-masi <eric.masi.dev@gmail.com>
2023-01-08 20:46:41 -0500

    Get hit players

--- src/stats/nhl.ts
+++ src/stats/nhl.ts
@@ -66,7 +66,13 @@ type Goals = {
 
 type PlayerElement = {
   player: Player;
-  playerType: 'Scorer' | 'Assist' | 'Goalie' | 'PenaltyOn' | 'DrewBy';
+  playerType:
+    | 'Scorer'
+    | 'Assist'
+    | 'Goalie'
+    | 'PenaltyOn'
+    | 'DrewBy'
+    | 'Hitter';
 };
 
 type Player = {
@@ -87,8 +93,8 @@ export async function updateNhlStats(ctx: Context, register: RegisterFn) {
   const scheduledGames = await getScheduledGames(ctx.prisma);
 
   // TODO: Delete after testing
-  // const test = true;
-  // if (test) return;
+  const test = true;
+  if (test) return;
 
   scheduledGames.forEach(({ game_date, game_pk, link }) => {
     register({
@@ -101,6 +107,7 @@ export async function updateNhlStats(ctx: Context, register: RegisterFn) {
           prevScoringPlays: [],
           prevPenaltyPlays: [],
           prevHitPlays: [],
+          prisma: ctx.prisma,
           end,
         }),
       invokeImmediately: false,
@@ -125,29 +132,45 @@ async function pollGameStats(config: {
   prevScoringPlays: number[];
   prevPenaltyPlays: number[];
   prevHitPlays: number[];
+  prisma: PrismaClient;
   end: () => void;
 }) {
-  const { url, prevScoringPlays, prevPenaltyPlays, end } = config;
+  const { url, prevScoringPlays, prevPenaltyPlays, prevHitPlays, prisma, end } =
+    config;
   const data = await fetchJson<GameFeed>(url, { timeout: 0, retry: 0 });
   const gameStates = ['Scheduled', 'In Progress'];
+
   if (!gameStates.includes(data.gameData.status.detailedState)) {
     // update game state
     end();
     return;
   }
 
-  const { teams } = data.gameData;
-  const { scoringPlays, penaltyPlays, allPlays } = data.liveData.plays;
+  const {
+    gamePk,
+    gameData: { teams },
+    liveData: {
+      plays: { scoringPlays, penaltyPlays, allPlays },
+    },
+  } = data;
 
   const scoringPlaysDiff = getPlayDiff(prevScoringPlays, scoringPlays);
-  const penaltyPlaysDiff = getPlayDiff(prevPenaltyPlays, penaltyPlays);
-  const scoringPlayValues = getValsByIndexes(scoringPlaysDiff, allPlays);
-  const penaltyPlayValues = getValsByIndexes(penaltyPlaysDiff, allPlays);
-
+  const scoringPlayValues = getValuesByIndexes(scoringPlaysDiff, allPlays);
   const scoringPlayers = getScoringPlayers(scoringPlayValues, teams);
+
+  const penaltyPlaysDiff = getPlayDiff(prevPenaltyPlays, penaltyPlays);
+  const penaltyPlayValues = getValuesByIndexes(penaltyPlaysDiff, allPlays);
   const penalizedPlayers = getPenalizedPlayers(penaltyPlayValues, teams);
 
-  const playersToBeUpdated = [...scoringPlayers, ...penalizedPlayers];
+  const hitPlayIndexes = getNewHitPlayIndexes(prevHitPlays, allPlays);
+  const hitPlayValues = getValuesByIndexes(hitPlayIndexes, allPlays);
+  const hitters = getHitters(hitPlayValues, teams);
+
+  const playersToBeUpdated = [
+    ...scoringPlayers,
+    ...penalizedPlayers,
+    ...hitters,
+  ].map((data) => ({ ...data, gamePk }));
 
   console.log(playersToBeUpdated);
 
@@ -157,17 +180,10 @@ async function pollGameStats(config: {
       prevScoringPlays: scoringPlaysDiff,
       prevPenaltyPlays: penaltyPlaysDiff,
       prevHitPlays: [],
+      prisma,
       end,
     });
   }, 10_000);
-}
-
-function getPlayDiff(prev: number[], curr: number[]) {
-  return curr.slice(prev.length);
-}
-
-function getValsByIndexes<T>(indexes: number[], arr: T[]) {
-  return indexes.map((idx) => arr[idx]);
 }
 
 function getScoringPlayers(plays: Play[], teams: Teams) {
@@ -223,6 +239,55 @@ function getPenalizedPlayers(plays: Play[], teams: Teams) {
   );
 }
 
+function getHitters(plays: Play[], teams: Teams) {
+  return plays.flatMap(({ players, team: { id: playerTeamId } }) => {
+    const hitter = getHitter(players);
+    const opponentTeamId = getOpponentTeam(playerTeamId, teams);
+
+    return [
+      {
+        playerTeamId,
+        opponentTeamId,
+        playerId: hitter?.player.id,
+        goals: 0,
+        points: 0,
+        assists: 0,
+        hits: 1,
+        penaltyMinutes: 0,
+      },
+    ];
+  });
+}
+
+function getNewHitPlayIndexes(prevHitPlays: number[], allPlays: Play[]) {
+  if (allPlays.length === 0) {
+    return [];
+  }
+
+  const idxAfterLastHitPlay = (prevHitPlays.at(-1) ?? 0) + 1;
+  const newHitPlayIndexes = [];
+
+  for (let i = allPlays.length - 1; i >= idxAfterLastHitPlay; i--) {
+    const { result } = allPlays[i];
+
+    if (result.event !== 'Hit') {
+      continue;
+    }
+
+    newHitPlayIndexes.push(i);
+  }
+
+  return newHitPlayIndexes;
+}
+
+function getPlayDiff(prev: number[], curr: number[]) {
+  return curr.slice(prev.length);
+}
+
+function getValuesByIndexes<T>(indexes: number[], arr: T[]) {
+  return indexes.map((idx) => arr[idx]);
+}
+
 function getOpponentTeam(playerTeamId: number, teams: Teams) {
   const {
     away: { id: awayTeamId },
@@ -241,4 +306,8 @@ function getAssistingPlayers(players: PlayerElement[]) {
 
 function getPenalizedPlayer(players: PlayerElement[]) {
   return players.find((player) => player.playerType === 'PenaltyOn');
+}
+
+function getHitter(players: PlayerElement[]) {
+  return players.find((player) => player.playerType === 'Hitter');
 }
