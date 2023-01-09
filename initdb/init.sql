fd80f1a17dc632ced06b57a7246e3555c49f8a95
git-masi <eric.masi.dev@gmail.com>
2023-01-09 07:26:20 -0500

    Move tables out of app schema

--- initdb/init.sql
+++ initdb/init.sql
@@ -33,9 +33,10 @@ CREATE TABLE IF NOT EXISTS api.player_stats (
 	penalty_minutes SMALLINT NOT NULL,
 	PRIMARY KEY(player_id, game_pk)
 );
-CREATE TABLE IF NOT EXISTS app.schedule (
-	-- Ideally we would want this to be of type `league_abrv` but prisma v4.8.1 can 't
+CREATE TABLE IF NOT EXISTS api.schedule (
+	-- Ideally we would want this to be of type `league_abrv` but prisma could not
 	-- introspect the composite key correctly. We can enforce this in application code.
+	-- This is an area for more research.
 	league TEXT NOT NULL,
 	game_pk INT NOT NULL,
 	game_date TIMESTAMPTZ NOT NULL,
@@ -43,21 +44,5 @@ CREATE TABLE IF NOT EXISTS app.schedule (
 	status TEXT NOT NULL,
 	PRIMARY KEY(league, game_pk)
 );
-CREATE TABLE IF NOT EXISTS app.last_play (
-	-- Ideally we would want this to be of type `league_abrv` but prisma v4.8.1 can 't
-	-- introspect the composite key correctly. We can enforce this in application code.
-	league TEXT NOT NULL,
-	game_pk INT NOT NULL,
-	last_play TIMESTAMPTZ NOT NULL,
-	PRIMARY KEY(league, game_pk)
-);
 -- Init indexes
-CREATE INDEX app_schedule_game_date ON app.schedule(game_date);
-CREATE INDEX app_last_play_last_play ON app.last_play(last_play);
--- Init the app role with all privileges
-GRANT USAGE ON SCHEMA api TO app;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA api TO app;
-GRANT ALL ON ALL TABLES IN SCHEMA api TO app;
-GRANT USAGE ON SCHEMA app TO app;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA app TO app;
-GRANT ALL ON ALL TABLES IN SCHEMA app TO app;
+CREATE INDEX app_schedule_game_date ON api.schedule(status);
